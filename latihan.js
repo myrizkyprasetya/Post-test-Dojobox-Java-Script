e0269070de1ff32787a29552218644fa66598bae
myrizkyprasetya <ifharizky28@gmail.com>
2021-08-05 18:57:34 +0700

    first commit

---
+++ latihan.js
@@ -0,0 +1,19 @@
+//yasifa
+alert('test');
+
+var nilai = prompt('Masukkan nilai *nilai diisi dengan angka 1-100 = ');
+if (nilai >= 70) {
+    document.write("Selamat Anda Lulus");
+}
+else {
+    document.write("Maaf Anda Tidak Lulus")
+}
+
+
+var nilai = prompt('Masukkan nilai = ');
+if (nilai >= 70) {
+    document.write("Selamat Anda Lulus");
+}
+else {
+    document.write("Maaf Anda Tidak Lulus")
+}
---
+++ latihanSwitch.js
@@ -0,0 +1,18 @@
+var nilai = parseInt(prompt('Masukkan nilai Anda = ','0-100'));
+
+switch(true){
+    case nilai>=90:
+        document.write('Grade A');
+        break;
+    case nilai>=80:
+        document.write('Grade B');
+        break;
+    case nilai>=70:
+        document.write('Grade C');
+        break;
+    case nilai>=60:
+        document.write('Grade D');
+        break;
+    default:
+        document.write('Grade E');
+}
